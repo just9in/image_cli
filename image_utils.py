6a561bdf6fddce470d46a74321358eb1902851a1
just9in <24sainihar@gmail.com>
2026-02-13 15:09:33 +0530

    Clear repository content

--- image_utils.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import imagehash  #using for generaition of fingerprints
-
-
-def generate_hash(image_path):
-    image = Image.open(image_path)
-    hash_value = imagehash.phash(image)
-    return str(hash_value)
